4b59ca34af13537705c264272c22192942315265
Rob <rzc1@staff.staffs.ac.uk>
2026-03-17 11:57:53 +0000

    Update paper

--- JOSS_utilities/prepare_paper.R
+++ JOSS_utilities/prepare_paper.R
@@ -11,22 +11,27 @@ kable_head <- function(x, ...) {
 
 md_style <- md_document("markdown_github", df_print = kable_head)
 attr <- xfun::attr2
-render("JOSS_utilities/paper.rmd", md_style, output_file = "paper.md", output_dir = getwd())
-
-# render("JOSS_utilities/paper.rmd", output_file = "pidpos.docx", output_dir = getwd())
-
+render("vignettes/paper-vignette.Rmd", md_style, output_file ="paper.md", output_dir = getwd())
 joss_preamble <- read_lines("JOSS_utilities/joss_preamble.txt")
 
 c(joss_preamble, read_lines("paper.md")) |> 
   stringr::str_replace_all("\\\\(\\[|\\])", "\\1") |>
   readr::write_lines("paper.md")
 
+# render("JOSS_utilities/paper.rmd", output_file = "pidpos.docx", output_dir = getwd())
 
-paper.md <- read_lines("JOSS_utilities/paper.rmd")
-vignette_preamble <- read_lines("JOSS_utilities/vignette_preamble.txt")
 
-c(vignette_preamble, paper.md) |>
-  readr::write_lines("vignettes/PaperVignette.Rmd")
-
-detach("package:pidpos", unload = TRUE)
-pkgdown::build_site()
+# 
+# c(joss_preamble, read_lines("paper.md")) |> 
+#   stringr::str_replace_all("\\\\(\\[|\\])", "\\1") |>
+#   readr::write_lines("paper.md")
+# 
+# 
+# # paper.md <- read_lines("JOSS_utilities/paper.rmd")
+# vignette_preamble <- read_lines("JOSS_utilities/vignette_preamble.txt")
+# 
+# c(vignette_preamble, paper.md) |>
+#   readr::write_lines("vignettes/PaperVignette.Rmd")
+# 
+# detach("package:pidpos", unload = TRUE)
+# pkgdown::build_site()
