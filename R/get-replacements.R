87ede66c203c4f03e6434d61c4b99a9cf96afa9d
Rob <rzc1@staff.staffs.ac.uk>
2026-03-18 16:59:34 +0000

    Utilities based on Mapper implementation

---
+++ R/get-replacements.R
@@ -0,0 +1,29 @@
+#' Access the cache of replacements
+#'  
+#' Tools for accessing the replacements inside a [make_replacement_function()] 
+#' function.
+#' 
+#' @param object A function built by [make_replacement_function()] having been used
+#' in [auto_replace()].
+#' @param key The string to lookup a replacement for.
+#' @param value The string to lookup what it replaced.
+#' 
+#' @name get-replacements
+NULL 
+
+#' @rdname get-replacements
+get_replacement_cache <- function(object){
+  attr(object, "mapper")$cache
+}
+
+#' @rdname get-replacements
+key_lookup <- function(object, key){
+  cache <- get_replacement_cache(object)
+  cache[[key]]
+}
+
+#' @rdname get-replacements
+value_lookup <- function(object, value){
+  cache <- get_replacement_cache(object)
+  names(which(cache == value))
+}
