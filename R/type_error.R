3a067f7a3bab55d78ad50e86b4405edc27b9de1e
Rob <rzc1@staff.staffs.ac.uk>
2026-03-18 17:00:07 +0000

    Implement package errors and warnings

--- R/type_error.R
+++ R/type_error.R
@@ -1,4 +1,10 @@
-#' A custom abort function
+#' Package errors and warnings
+#'
+#' To assist with ... the package has implemented several custom errors and warnings 
+#' which are embedded as safeguards in the function factories.  The intention is to allow 
+#' users to catch specific error types raised in the package structure separately to 
+#' any raised from custom code.
+#' 
 #'
 #' @param message The error message to display
 #' @param ... Additional arguments to pass to `abort()`
@@ -8,14 +14,32 @@
 #'
 #' @importFrom rlang abort caller_env
 #' @keywords internal
+#' @name custom-errors-warnings
 #'
-#' @examples
-#' #' # Example of using type_error
-#' f <- function() {
-#'   pidpos:::type_error("This is a type error")
-#' }
-#'
-#' # Trigger the error safely without stopping R
-#' result <- try(f(), silent = TRUE)
-#' result
+NULL
+
+#' @rdname custom-errors-warnings
 type_error <- new_error_type("type_error")
+
+#' @rdname custom-errors-warnings
+exceeded_max_error <- new_error_type("exceeded_max_error")
+
+#' @rdname custom-errors-warnings
+exceeded_half_max_warn <- new_warn_type("exceed_half_max_warning")
+
+#' @rdname custom-errors-warnings
+iteration_warn <- new_warn_type("iteration_warning")
+
+
+escalate <- function(w, elevate_warnings) {
+  .class <- class(w)
+  if (elevate_warnings) {
+    .class <- gsub("warning$", "error", .class)
+    rlang::abort(w$message, class = .class)
+  } else {
+    rlang::warn(w$message, class = .class)
+    #invokeRestart("muffleWarning") 
+  }
+}
+
+
