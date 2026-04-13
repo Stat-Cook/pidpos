f8a37a0647cf5d98f9c57e455ff506245d1cd4e9
Stat-Cook <Stat-Cook@users.noreply.github.com>
2026-04-13 15:08:06 +0000

    Style code (GHA)

--- R/tag_documents.R
+++ R/tag_documents.R
@@ -34,7 +34,7 @@
 #' # Create a tagger for the English LINES model
 #' lines_tagger <- udpipe_factory("english-lines")
 #' lines_result <- pidpos:::tag_documents(example_text, tagger = lines_tagger)
-#'}
+#' }
 tag_documents <- function(docs,
                           doc_ids = NULL,
                           tagger = NULL,
