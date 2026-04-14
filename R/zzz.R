d7cbdf5836fc82058486ee4ffe7c7372b80d01d3
Rob <rzc1@staff.staffs.ac.uk>
2026-04-14 15:48:45 +0100

    deprecate definitions

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@ pidpos_env <- new.env()
   op <- options()
   if (is.null(op[["pidpos_context_window"]])) options(pidpos_context_window = 25)
 
-  pidpos_env$model_folder <- pidpos_env$deault_model_folder
+  # pidpos_env$model_folder <- pidpos_env$default_model_folder
 
   pidpos_env$allowed_repos <- c(
     `2.5` = "jwijffels/udpipe.models.ud.2.5",
