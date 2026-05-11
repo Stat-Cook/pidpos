379732fcc586c7f81236c7d94411b4056f180285
Stat-Cook <Stat-Cook@users.noreply.github.com>
2026-05-11 15:04:55 +0000

    Style code (GHA)

--- tests/testthat/test-check_model_download_consent.R
+++ tests/testthat/test-check_model_download_consent.R
@@ -1,6 +1,6 @@
 test_that("check_model_download_consent returns TRUE if option set", {
   withr::defer(options(pidpos_download_approved = NULL))
-  
+
   options(pidpos_download_approved = TRUE)
   expect_true(check_model_download_consent("en_core_web_lg"))
 })
@@ -10,7 +10,7 @@ test_that("check_model_download_consent returns TRUE if env var set", {
     Sys.unsetenv("PIDPOS_DOWNLOAD_APPROVED")
     options(pidpos_download_approved = NULL)
   })
-  
+
   options(pidpos_download_approved = FALSE)
   Sys.setenv(PIDPOS_DOWNLOAD_APPROVED = "true")
   expect_true(check_model_download_consent("en_core_web_lg"))
@@ -21,7 +21,7 @@ test_that("check_model_download_consent option takes priority over env var", {
     Sys.unsetenv("PIDPOS_DOWNLOAD_APPROVED")
     options(pidpos_download_approved = NULL)
   })
-  
+
   options(pidpos_download_approved = TRUE)
   Sys.setenv(PIDPOS_DOWNLOAD_APPROVED = "false")
   expect_true(check_model_download_consent("en_core_web_lg"))
@@ -32,14 +32,14 @@ test_that("check_model_download_consent errors if user declines", {
     options(pidpos_download_approved = NULL)
     options(pidpos_caching = NULL)
   })
-  
+
   options(pidpos_download_approved = FALSE)
   options(pidpos_caching = TRUE)
   Sys.unsetenv("PIDPOS_DOWNLOAD_APPROVED")
-  
+
   mockery::stub(check_model_download_consent, "interactive", function() TRUE)
   mockery::stub(check_model_download_consent, "readline", function(...) "n")
-  
+
   expect_error(
     check_model_download_consent("en_core_web_lg"),
     "Download cancelled"
@@ -51,15 +51,14 @@ test_that("check_model_download_consent sets option after consent", {
     options(pidpos_download_approved = NULL)
     options(pidpos_caching = NULL)
   })
-  
+
   options(pidpos_download_approved = FALSE)
   options(pidpos_caching = TRUE)
   Sys.unsetenv("PIDPOS_DOWNLOAD_APPROVED")
-  
+
   mockery::stub(check_model_download_consent, "interactive", function() TRUE)
   mockery::stub(check_model_download_consent, "readline", function(...) "y")
-  
+
   check_model_download_consent("en_core_web_lg")
   expect_true(getOption("pidpos_download_approved"))
 })
-
