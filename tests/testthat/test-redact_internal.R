76e5e089758f24947379b4b46b2efc8ed32e727b
Stat-Cook <Stat-Cook@users.noreply.github.com>
2026-06-02 11:02:38 +0000

    Style code (GHA)

--- tests/testthat/test-redact_internal.R
+++ tests/testthat/test-redact_internal.R
@@ -1,10 +1,9 @@
 test_that("redact_internal methods", {
-  
   expect_output(redact_internal("Test message", print), "Test message")
-  
+
   frm <- data.frame(LETTERS)
   redacted <- redact_internal(frm, tolower)
-  
+
   expect_equal(dim(redacted), c(26, 1))
   expect_true(all(redacted$LETTERS == letters))
 })
