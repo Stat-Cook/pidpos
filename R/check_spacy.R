c116c0d77828b9c0b592249fdfd03855d3971489
Rob <rzc1@staff.staffs.ac.uk>
2026-06-02 10:20:49 +0100

    alter imports

--- R/check_spacy.R
+++ R/check_spacy.R
@@ -1,13 +1,20 @@
+#' @importFrom stats setNames
+#'
 check_spacy <- function() {
-  py_packages <- c("spacy", "en_core_web_lg", "en_core_web_trf") %>%
-    setNames(., .)
-
+  py_packages <- c(
+    spacy = "spacy",
+    en_core_web_lg = "en_core_web_lg",
+    en_core_web_trf = "en_core_web_trf"
+  )
+  
   missing <- !sapply(py_packages, reticulate::py_module_available)
-
+  
   if (sum(missing) > 0) {
     msg <- paste0("`", names(which(missing)), "`", collapse = ", ")
     stop(
-      "The python packages ", msg, " are missing. ",
+      "The python packages ",
+      msg,
+      " are missing. ",
       "To check your active python env run `reticulate::py_config()`."
     )
   }
