e9e664a284ce7c2fdd6da72984dc390b129d6809
Stat-Cook <Stat-Cook@users.noreply.github.com>
2026-06-02 13:56:34 +0000

    Style code (GHA)

--- R/regex_factory.R
+++ R/regex_factory.R
@@ -55,7 +55,7 @@ pid_patterns <- tibble::tribble(
 #'
 #' @return A function with signature \code{function(doc, doc_id)} that returns
 #'   a data frame with columns: doc_id, type, match, start, end, doc.
-#'   
+#'
 #' @export
 regex_factory <- function(patterns = pid_patterns) {
   #  Validate patterns at construction time
