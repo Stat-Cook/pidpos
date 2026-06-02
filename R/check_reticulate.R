41439ce0a07130a04790ffd6c65fbdecce127d17
Rob <rzc1@staff.staffs.ac.uk>
2026-06-02 14:10:23 +0100

    convert to rlang version

--- R/check_reticulate.R
+++ R/check_reticulate.R
@@ -1,8 +1,6 @@
 check_reticulate <- function() {
-  if (!requireNamespace("reticulate", quietly = TRUE)) {
-    rlang::abort(
-      "reticulate is not installed.\n",
-      "Install with: install.packages('reticulate')"
-    )
-  }
+  rlang::check_installed(
+    "reticulate",
+    reason = "to use this function"
+  )
 }
