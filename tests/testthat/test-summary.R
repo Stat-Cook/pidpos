a300e5408b0ffd6337eedc77ddb4ab88146b4a41
Stat-Cook <Stat-Cook@users.noreply.github.com>
2026-06-04 15:04:46 +0000

    Style code (GHA)

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -5,14 +5,12 @@ test_that("summary works", withr::with_options(list(pidpos_download_approved = T
     `Sentence` = "Bob came to visit.",
     `Document` = "Bob came to visit.",
     `Repeats` = 1,
-    `Affected Columns`  = c("`Text`")
+    `Affected Columns` = c("`Text`")
   )
   class(report) <- c("pidpos", class(report))
-  
+
   .summary <- summary(report)
 
   expect_equal(nrow(.summary), 1)
   expect_equal(ncol(.summary), 4)
 }))
-
-
