862c912c1d54cc3c45843d10b678c3daed29f984
Rob <rzc1@staff.staffs.ac.uk>
2026-06-04 16:03:25 +0100

    Refactor test suite.

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,7 +1,18 @@
 test_that("summary works", withr::with_options(list(pidpos_download_approved = TRUE), {
-  report <- pidpos(the_one_in_massapequa)
+  report <- tibble::tibble(
+    ID = 1,
+    `Token` = "Bob",
+    `Sentence` = "Bob came to visit.",
+    `Document` = "Bob came to visit.",
+    `Repeats` = 1,
+    `Affected Columns`  = c("`Text`")
+  )
+  class(report) <- c("pidpos", class(report))
+  
   .summary <- summary(report)
 
-  expect_equal(nrow(.summary), 2)
+  expect_equal(nrow(.summary), 1)
   expect_equal(ncol(.summary), 4)
 }))
+
+
