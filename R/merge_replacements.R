e3c28667d0c9a781c65f589b791a0a02adc63583
Rob <rzc1@staff.staffs.ac.uk>
2026-06-11 13:13:42 +0100

    Mark as in development

--- R/merge_replacements.R
+++ R/merge_replacements.R
@@ -18,13 +18,17 @@ vector_merge_redactions <- function(vec,
 
 #' Remove PID from a data frame via a merge
 #'
+#' Higher-speed alternative to the function-based redaction pipeline for
+#' reapplying existing redaction rules. Not yet benchmarked against the
+#' standard pipeline - kept internal until performance advantage is confirmed.
+#'
 #' @param frm The data frame to be redacted
 #' @param cached_redactions A data frame with `If` and `Then` columns
 #' @param preprocess A function of preprocessing steps to be applied to the text columns.
 #' 
 #' @return A data frame of redacted data
 #' 
-#' @export
+#' @keywords internal
 merge_redactions <- function(frm,
                              cached_redactions,
                              preprocess = utf8::utf8_encode) {
